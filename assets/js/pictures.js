18d4615420b4eeca32c1a77b7ad5bc0112032cde
Sebastian Senf <contact@mustardamus.com>
2016-01-30 14:10:47 +0100

    split cities nav, styled them

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -4,4 +4,14 @@ $(function() {
     closeBtn: false,
     helpers : { overlay: { locked: false }}
   });
+
+  var citiesNavEl = $('#cities-nav');
+  var citiesNum   = $('.first-row li', citiesNavEl).length;
+  var halfNum     = Math.floor(citiesNum / 2);
+
+  for(var i = 0; i < citiesNum - halfNum; i++) {
+    var el = $('.first-row li:last-child', citiesNavEl);
+    console.log(el);
+    $('.second-row', citiesNavEl).append(el);
+  }
 });
