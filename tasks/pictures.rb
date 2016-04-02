8de25f060f5d1a4d6a1ca2db4c5710b03afeec5a
Sebastian Senf <contact@mustardamus.com>
2016-04-03 00:29:51 +0200

    pisa and rome pictures, dont generate images if they are already there. time is money boy.

--- tasks/pictures.rb
+++ tasks/pictures.rb
@@ -10,9 +10,9 @@ thumb_width   = 200
 pic_width     = 1000
 out_arr       = []
 
-if Dir.exists?(out_dir)
-  FileUtils.rm_rf(out_dir)
-end
+#if Dir.exists?(out_dir)
+  #FileUtils.rm_rf(out_dir)
+#end
 
 Dir.glob "#{in_dir}/**" do |country_path|
   Dir.glob "#{country_path}/**" do |city_path|
@@ -26,21 +26,24 @@ Dir.glob "#{in_dir}/**" do |country_path|
       filename     = picture_path.split('/').last
       thumbname    = "#{filename.split('.').first}_thumb.#{filename.split('.').last}"
       out_path     = "#{out_dir}/#{obj[:country]}/#{obj[:city]}"
-      image        = MiniMagick::Image.open(picture_path)
-      thumb_height = image.height * thumb_width / image.width + 1
-      pic_height   = image.height * pic_width / image.width + 1
 
-      FileUtils.mkdir_p out_path
+      unless File.file?("#{out_path}/#{filename}")
+        image        = MiniMagick::Image.open(picture_path)
+        thumb_height = image.height * thumb_width / image.width + 1
+        pic_height   = image.height * pic_width / image.width + 1
 
-      image.resize "#{pic_width}x#{pic_height}"
-      image.write "#{out_path}/#{filename}"
+        FileUtils.mkdir_p out_path
 
-      image.resize "#{thumb_width}x#{thumb_height}"
-      image.quality 80
-      image.write "#{out_path}/#{thumbname}"
+        image.resize "#{pic_width}x#{pic_height}"
+        image.write "#{out_path}/#{filename}"
 
-      obj[:pictures] << filename
-      puts "#{obj[:country]} - #{obj[:city]} - #{filename}"
+        image.resize "#{thumb_width}x#{thumb_height}"
+        image.quality 80
+        image.write "#{out_path}/#{thumbname}"
+
+        obj[:pictures] << filename
+        puts "#{obj[:country]} - #{obj[:city]} - #{filename}"
+      end
     end
 
     out_arr << obj
